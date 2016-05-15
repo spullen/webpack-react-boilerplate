87a7f0d50461352767786fcf70c59d56b091f7db
Scott Pullen <s.pullen05@gmail.com>
2016-05-15 12:16:09 -0400

    Needed to tweak some prod webpack configurations

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,53 @@
+var path = require('path');
 var webpack = require('webpack');
-var webpackConfig = require('./webpack.config');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var plugins = [
-  new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: JSON.stringify('production')}
-    }
-  ),
-];
-
-webpackConfig.debug = false;
-webpackConfig.devtool = 'source-map';
-webpackConfig.plugins = webpackConfig.plugins.concat(plugins)
-
-module.exports = webpackConfig;
+module.exports = {
+  debug: false,
+  devtool: 'source-map',
+  entry: [
+    './src/app.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+        'process.env': {NODE_ENV: JSON.stringify('production')}
+      }
+    ),
+  ],    
+  module: {
+    loaders: [
+      {
+        test: /\.(html)$/,
+        loader: "file?name=[path][name].[ext]&context=./src/"
+      },
+      {
+        test: /\.scss$/,
+        include: [
+            path.resolve(__dirname, "src/styles")
+        ],
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css', 'sass')
+      },
+      {
+        test: /\.js?$/,
+        include: path.join(__dirname, 'src'),
+        exclude: /node_modules/,
+        loader: ['babel'],
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss']
+  }
+};
